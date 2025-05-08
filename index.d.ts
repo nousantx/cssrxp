f428bf9ac25ec643a7905d2ea67c028fdac168cf
nousantx <nousantx@gmail.com>
2025-05-08 09:44:35 +0700

    feat: release v0.2.0

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
-export declare const is: {
-    [type: string]: RegExp;
-};
-export default is;
+type RegExpType = { [type: string]: RegExp }
+
+export declare const is: RegExpType
+export declare const has: RegExpType
+
+export default { is, has }
